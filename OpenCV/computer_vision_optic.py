065ca86c29c5b9f89ac1691e8f71ca9e1bec35ea
JHa568 <jamieha9@gmail.com>
2021-05-23 12:27:49 +1000

    Added Dense and sparse flow

--- OpenCV/computer_vision_optic.py
+++ OpenCV/computer_vision_optic.py
@@ -1,23 +1,32 @@
 import numpy as np
+from low_level_vision.optical_flow import optical_flow
 import cv2 as cv
 
 cap = cv.VideoCapture(cv.samples.findFile("security.mp4"))
-ret, frame1 = cap.read()
-prvs = cv.cvtColor(frame1,cv.COLOR_BGR2GRAY)
-hsv = np.zeros_like(frame1)
+
+# Create some random colors
+color = np.random.randint(0,255,(100,3))
+# Take first frame and find corners in it
+ret, old_frame = cap.read()
+old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
+# Create a mask image for drawing purposes
+mask = np.zeros_like(old_frame)
+
+hsv = np.zeros_like(old_frame)
 hsv[...,1] = 255
 
-while(1):
-    ret, frame2 = cap.read()
-    next = cv.cvtColor(frame2,cv.COLOR_BGR2GRAY)
-    flow = cv.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-    mag, ang = cv.cartToPolar(flow[...,0], flow[...,1])
-    hsv[...,0] = ang*180/np.pi/2
-    hsv[...,2] = cv.normalize(mag,None,0,255,cv.NORM_MINMAX)
-    bgr = cv.cvtColor(hsv,cv.COLOR_HSV2BGR)
-    cv.imshow('frame2', bgr)
-    cv.imshow('frame1', frame2)
+optical_flow = optical_flow()
+while True:
+    ret,frame = cap.read()
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # calculate optical flow
+    # optical_flow.Denseflow(old_gray, frame_gray, hsv)#
+
+    flow_img = optical_flow.Sparseflow(frame, old_frame, old_gray, frame_gray)
+    cv.imshow('frame', flow_img)
+
     k = cv.waitKey(30) & 0xff
     if k == 27:
         break
-    prvs = next
+    # Now update the previous frame and previous points
+    old_gray = frame_gray.copy()
--- OpenCV/low_level_vision/optical_flow.py
+++ OpenCV/low_level_vision/optical_flow.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 '''
 For a low level vision task.
@@ -6,8 +7,52 @@ Can be used for detecting the motion in the camera.
 '''
 
 class optical_flow():
-    def flow(self, prev_img, next_img):
-        # This will show the previous movement of
-        # the object or thing in the camera plane
+    def __init__(self):
+        self.good_new = []
+        self.good_old = []
 
-        return None
+    def Denseflow(self, prev_img, next_img, hsv, flow=None, pry_scale=0.5, lvl=3, winsize=15,
+                                                                           iterations=3,
+                                                                           poly_n=5,
+                                                                           poly_sigma=1.2,
+                                                                           flags=0):
+
+        # This will show the previous movement of
+        flow = cv.calcOpticalFlowFarneback(prev_img, next_img, flow, pry_scale, lvl, winsize, iterations, poly_n, poly_sigma, flags)
+        # Changes the flow to polar coordinates
+        mag, ang = cv.cartToPolar(flow[...,0], flow[...,1])
+        # Makes the hue value ang*180/np.pi/2
+        hsv[...,0] = ang*180/np.pi/2
+        # Makes the value is normalised.
+        hsv[...,2] = cv.normalize(mag,None,0,255,cv.NORM_MINMAX)
+        bgr = cv.cvtColor(hsv,cv.COLOR_HSV2BGR)
+        return bgr
+
+    def Sparseflow(self, frame, old_frame, old_gray, frame_gray):
+            # Create some random colors
+            color = np.random.randint(0,255,(100,3)) # tracker markers
+            p0 = cv.goodFeaturesToTrack(old_gray, mask = None, maxCorners = 100,
+                                                               qualityLevel = 0.3,
+                                                               minDistance = 7,
+                                                               blockSize = 7 )
+            # Create a mask image for drawing purposes
+            mask = np.zeros_like(old_frame)
+
+            p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, winSize  = (15,15),
+                                                                                  maxLevel = 2,
+                                                                                  criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
+            # Select good points
+            if p1 is not None:
+                self.good_new = p1[st==1]
+                self.good_old = p0[st==1]
+
+            # draw the tracks
+            for i,(new,old) in enumerate(zip(self.good_new, self.good_old)):
+                a,b = new.ravel()
+                c,d = old.ravel()
+                mask = cv.line(mask, (int(a),int(b)),(int(c),int(d)), color[i].tolist(), 2)
+                frame = cv.circle(frame,(int(a),int(b)),5,color[i].tolist(),-1)
+
+            img = cv.add(frame,mask)
+            p0 = self.good_new.reshape(-1,1,2)
+            return img
